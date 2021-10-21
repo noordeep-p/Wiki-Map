06d31490ab94af5e4c33b6ba681c2033c4d1665a
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2021-10-21 14:54:39 -0400

    create seed data for maps

---
+++ db/seeds/02_maps.sql
@@ -0,0 +1,7 @@
+-- Maps seed data
+
+INSERT INTO maps (user_id, name, description, image_url) VALUES (1,'Best restaurants in Toronto', 'Here is a list of all my fav restaurants in Toronto', 'https://tinyurl.com/47mwc4tt');
+INSERT INTO maps (user_id, name, description, image_url) VALUES (2,'Best retaurants in Vancouver', 'Here is a list of all my fav restaurants in Vancouver', 'https://tinyurl.com/5833j6sa');
+INSERT INTO maps (user_id, name, description, image_url) VALUES (1,'Best outdoor activities Toronto', 'a list of all my fav outdoor activity spots', 'https://tinyurl.com/47mwc4tt');
+INSERT INTO maps (user_id, name, description, image_url) VALUES (2,'Best beaches in Vancouver', 'Here is a list of the best beaches in Vancouver', 'https://tinyurl.com/5833j6sa');
+INSERT INTO maps (user_id, name, description, image_url) VALUES (1,'Best hiking trails Toronto', 'Here is a list of all my fav hiking trails in Toronto', 'https://tinyurl.com/47mwc4tt');
