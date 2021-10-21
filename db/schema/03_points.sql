794320337e08b5dbb510ade0a4efa0d127654a2c
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2021-10-21 01:36:31 -0400

    create map points table

---
+++ db/schema/03_points.sql
@@ -0,0 +1,15 @@
+-- Drop and recreate points table
+
+DROP TABLE IF EXISTS points CASCADE;
+CREATE TABLE points (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES user(id) ON DELETE CASCADE,
+  map_id INTEGER REFERENCES map(id) ON DELETE CASCADE,
+  title VARCHAR(255) NOT NULL,
+  description TEXT,
+  image_url VARCHAR(255) NOT NULL,
+  address VARCHAR(255) NOT NULL,
+  latitude DECIMAL NOT NULL,
+  longitude DECIMAL NOT NULL
+)
+
