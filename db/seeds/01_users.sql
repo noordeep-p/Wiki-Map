6182c4109da5cef2e84ba2faaeed10dec0e5f6f2
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2021-10-21 14:54:23 -0400

    create seed data for users

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,7 @@
--- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+-- Users seed data
+
+INSERT INTO users (name,email,password) VALUES ('noor' , 'noor@example.com', 'password');
+INSERT INTO users (name,email,password) VALUES ('rhea' , 'rhea@example.com', 'password');
+INSERT INTO users (name,email,password) VALUES ('ron' , 'ron@example.com', 'password');
+INSERT INTO users (name,email,password) VALUES ('tom' , 'tom@example.com', 'password');
+INSERT INTO users (name,email,password) VALUES ('harry' , 'harry@example.com', 'password');
