7835d3888e289946e0919209e97d79b93bd9a5ac
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2021-10-21 15:11:08 -0400

    update DROP TABLE order to avoid FK constraints

--- db/schema/02_maps.sql
+++ db/schema/02_maps.sql
@@ -1,6 +1,5 @@
 -- Drop and recreate maps table
 
-DROP TABLE IF EXISTS maps CASCADE;
 CREATE TABLE maps (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
--- db/schema/03_points.sql
+++ db/schema/03_points.sql
@@ -1,10 +1,9 @@
 -- Drop and recreate points table
 
-DROP TABLE IF EXISTS points CASCADE;
 CREATE TABLE points (
   id SERIAL PRIMARY KEY NOT NULL,
-  user_id INTEGER REFERENCES user(id) ON DELETE CASCADE,
-  map_id INTEGER REFERENCES map(id) ON DELETE CASCADE,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  map_id INTEGER REFERENCES maps(id) ON DELETE CASCADE,
   title VARCHAR(255) NOT NULL,
   description TEXT,
   image_url TEXT,
