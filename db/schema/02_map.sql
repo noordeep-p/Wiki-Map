b2c2af9751d3d60bdeca7a31150768d93d823b4d
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2021-10-21 01:02:38 -0400

    create maps table

---
+++ db/schema/02_map.sql
@@ -0,0 +1,10 @@
+-- Drop and recreate maps table
+
+DROP TABLE IF EXISTS maps CASCADE;
+CREATE TABLE maps (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id),
+  name VARCHAR(255) NOT NULL,
+  description TEXT,
+  image_url VARCHAR(255) NOT NULL,
+);
