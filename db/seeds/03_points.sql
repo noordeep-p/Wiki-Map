c55c7ca09f53eab29ec6bf00433d5e8ed1a1d78c
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2021-10-21 14:54:55 -0400

    create seed data for map points

---
+++ db/seeds/03_points.sql
@@ -0,0 +1,17 @@
+-- Points seed data
+
+--        MAP 1(belongs to user 1): points
+INSERT INTO points (user_id, map_id, title, description, image_url, address, latitude, longitude) VALUES (1,1,'Ricks Good Eats','my number 1 food spot', 'https://tinyurl.com/47mwc4tt', 'foodspot1_address', 43.6488, -79.6870);
+INSERT INTO points (user_id, map_id, title, description, image_url, address, latitude, longitude) VALUES (1,1,'Casa Loma','my number 2 food spot', 'https://tinyurl.com/47mwc4tt', 'foodspot2_address', 43.6780, -79.4094);
+--        MAP 2(belongs to user 2): points
+INSERT INTO points (user_id, map_id, title, description, image_url, address,latitude,longitude) VALUES (2,2,'Pin Pin','my number 1 food spot', 'https://tinyurl.com/5833j6sa', 'foodspot1_address' , 49.2289, -123.0910);
+INSERT INTO points (user_id, map_id, title, description, image_url, address,latitude,longitude) VALUES (2,2,'Miku Vancouver','my number 2 food spot', 'https://tinyurl.com/5833j6sa', 'foodspot2_address', 49.2870, -123.1130);
+--        MAP 3(belongs to user 1): points
+INSERT INTO points (user_id, map_id, title, description, image_url, address, latitude, longitude) VALUES (1,3,'GlowZone 360','my number 1 activity spot', 'https://tinyurl.com/47mwc4tt', 'activity1_address', 43.6845, -79.7827);
+INSERT INTO points (user_id, map_id, title, description, image_url, address, latitude, longitude) VALUES (1,3,'Bad Axe Throwing','my number 2 activity spot', 'https://tinyurl.com/47mwc4tt', 'activity2_address', 43.6259 ,-79.6313);
+--        MAP 4(belongs to user 2): points
+INSERT INTO points (user_id, map_id, title, description, image_url, address, latitude, longitude) VALUES (2,4,'Acadia Beach','my number 1 beach spot', 'https://tinyurl.com/5833j6sa', 'beach1_address', 49.2805, -123.2422);
+INSERT INTO points (user_id, map_id, title, description, image_url, address, latitude, longitude) VALUES (2,4,'Spanish Banks Beach Park','my number 2 beach spot', 'https://tinyurl.com/5833j6sa', 'beach2_address', 49.2763, -123.2133);
+--        MAP 5(belongs to user 1): points
+INSERT INTO points (user_id, map_id, title, description, image_url, address, latitude, longitude) VALUES (1,5,'Crothers Woods','my number 1 hiking spot', 'https://tinyurl.com/47mwc4tt', 'hiking1_address', 43.6922, -79.3636);
+INSERT INTO points (user_id, map_id, title, description, image_url, address, latitude, longitude) VALUES (1,5,'West Humber Recreational Trail','my number 2 hiking spot', 'https://tinyurl.com/47mwc4tt', 'hiking2_address', 43.7341, -79.5613);
