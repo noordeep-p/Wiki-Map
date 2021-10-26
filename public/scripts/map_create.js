ed35144877b9a82b66d62cc317950b275440bc84
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2021-10-26 14:30:07 -0400

    implement markers and split mapOptions into seperate vars

--- public/scripts/map_create.js
+++ public/scripts/map_create.js
@@ -2,12 +2,32 @@
 /* eslint-disable func-style */
 /* eslint-disable no-undef */
 
-let map;
 
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 2,
-    center: new google.maps.LatLng(2.8, -187.3),
-    mapTypeId: "terrain",
+
+  let mapOptions = {
+    center: {lat:43.651070,lng:-79.347015},
+    zoom: 10,
+  };
+
+  // new map
+  let newMap = new
+  google.maps.Map(document.getElementById('map'), mapOptions);
+
+  // add marker
+  let marker = new
+  google.maps.Marker({
+    position: {lat: 43.6488, lng: -79.6870},
+    map: newMap
   });
+
+  // add popup window
+  let infoWindow = new google.maps.InfoWindow(
+    {content: "<h1>Ricks Good Eats</h1>",
+    });
+
+  marker.addListener('click', function() {
+    infoWindow.open(newMap, marker);
+  });
+
 }
