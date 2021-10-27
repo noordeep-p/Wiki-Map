96a1bdb58819febd7b7fa01ed25d5330d9b7da03
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2021-10-27 16:02:46 -0400

    add function retrieve map points from the server and add them to the map

--- public/scripts/map_create.js
+++ public/scripts/map_create.js
@@ -14,20 +14,33 @@ function initMap() {
   let newMap = new
   google.maps.Map(document.getElementById('map'), mapOptions);
 
-  // add marker
-  let marker = new
-  google.maps.Marker({
-    position: {lat: 43.6488, lng: -79.6870},
-    map: newMap
-  });
+  // get all markers from backed server and add then to the map
+  const addPointsByMapId = (mapId) => {
+    pointsByMapId(mapId).then(pointData => {
+      pointData.points.forEach(point => {
+        // add point
+        let marker = new
+        google.maps.Marker({
+          position: {lat: parseFloat(point.latitude), lng: parseFloat(point.longitude)},
+          map: newMap
+        });
 
-  // add popup window
-  let infoWindow = new google.maps.InfoWindow(
-    {content: "<h1>Ricks Good Eats</h1>",
+        // add point pop window with HTML markup
+        let infoWindow = new google.maps.InfoWindow(
+          {content: `<h1>${point.title}</h2>
+          <p>${point.description}</p>
+          <img src="${point.image_url}" alt= "Place Image" width="250" height="250">`,
+          });
+
+        // add click listener to point to display popup when clicked
+        marker.addListener('click', function() {
+          infoWindow.open(newMap, marker);
+
+        });
+      });
     });
+  };
 
-  marker.addListener('click', function() {
-    infoWindow.open(newMap, marker);
-  });
+  addPointsByMapId(1);
 
 }
