d969ed28c3aaca328c776f8115b303fb77c18419
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2021-10-29 01:26:41 -0400

    add routes to display and create new maps

--- routes/mapRoutes.js
+++ routes/mapRoutes.js
@@ -14,6 +14,15 @@ module.exports = (db) => {
    * ALL EXPRESS SERVER MAP ROUTES
    */
 
+  router.get("/display/:mapId", (req, res) => {
+    let user = req.session.user;
+    let mapId = req.params.mapId;
+    if (user) {
+      return res.render("map_edit", {mapId, user});
+    }
+    return res.render("maps_index", { user: null });
+  });
+
   router.get("/contributions", (req, res) => {
     let user = req.session.user;
     if (user) {
@@ -33,7 +42,27 @@ module.exports = (db) => {
   router.get("/create", (req, res) => {
     let user = req.session.user;
     if (user) {
-      return res.render("maps_create", user);
+      return res.render("maps_new", user);
+    }
+    return res.render("maps_index", { user: null });
+  });
+
+  router.post("/create", (req, res) => {
+    let userInfo = req.session.user;
+    let userId = userInfo.user.id;
+    let { name , description, image } = req.body;
+    let query = `INSERT INTO maps (user_id, name, description, image_url) VALUES ($1,$2,$3,$4) RETURNING *`;
+    if (userInfo) {
+      return db.query(query, [userId,name,description,image])
+        .then(data => {
+          if (data) {
+            let user = userInfo;
+            let mapId = data.rows[0].id;
+            return res.render("map_edit", { user, mapId });
+          }
+        }).then(
+        )
+        .catch(e => console.log(e));
     }
     return res.render("maps_index", { user: null });
   });
